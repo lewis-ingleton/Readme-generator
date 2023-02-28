c46d9cbc634eb16b9c52102603d959dcb7237b24
Lewis Ingleton <lewis.ingleton1@.com>
2023-02-28 15:44:39 +0000

    full list of questions (commented out)

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const inquirer = require('inquirer')
 const fs = require('fs');
-// const util = require('util');
+
+const generateMarkdown = (response) => {
+    return `# ${response.title}`
+
+        // `## Project description`,
+        // `${response.description}`
+
+        // `## Table of contents`,
+        // `${response.contents}`,
+
+        // `## Installation instructions`,
+        // `${response.instructions}`,
+
+        // `## Usage`,
+        // `${response.usage}`,
+
+        // `## Lisence`,
+        // `${response.lisence}`,
+
+        // `## Contribute`,
+        // `${response.contribute}`
+
+}
 
 console.log('readme gen running')
 
@@ -8,7 +30,12 @@ inquirer
     .prompt([
         {
             type: 'input',
-            message: 'Give your README a title',
+            message: 'Give your README a filename (dont include file-extension)',
+            name: 'filename',
+        },
+        {
+            type: 'input',
+            message: 'Project title',
             name: 'title',
         },
         {
@@ -16,48 +43,51 @@ inquirer
             message: 'Project description',
             name: 'description',
         },
-        {
-            type: 'input',
-            message: 'Table of contents',
-            name: 'contents',
-        },
-        {
-            type: 'input',
-            message: 'Installation instructions',
-            name: 'installation',
-        },
-        {
-            type: 'input',
-            message: 'Usage',
-            name: 'usage',
-        },
-        {
-            type: 'list',
-            message: 'Lisence',
-            name: 'lisence',
-            choices: ['MIT', 'GPLv2', 'Apache', 'GPLv3', 'BSD 3-clause']
-        },
-        {
-            type: 'input',
-            message: 'Contribute',
-            name: 'contribute',
-        },
-        {
-            type: 'input',
-            message: 'Tests',
-            name: 'tests',
-        },
-        {
-            type: 'input',
-            message: 'Questions',
-            name: 'questions',
-        },
+        // {
+        //     type: 'input',
+        //     message: 'Table of contents',
+        //     name: 'contents',
+        // },
+        // {
+        //     type: 'input',
+        //     message: 'Installation instructions',
+        //     name: 'installation',
+        // },
+        // {
+        //     type: 'input',
+        //     message: 'Usage',
+        //     name: 'usage',
+        // },
+        // {
+        //     type: 'list',
+        //     message: 'Lisence',
+        //     name: 'lisence',
+        //     choices: ['MIT', 'GPLv2', 'Apache', 'GPLv3', 'BSD 3-clause']
+        // },
+        // {
+        //     type: 'input',
+        //     message: 'Contribute',
+        //     name: 'contribute',
+        // },
+        // {
+        //     type: 'input',
+        //     message: 'Tests',
+        //     name: 'tests',
+        // },
+        // {
+        //     type: 'input',
+        //     message: 'Questions',
+        //     name: 'questions',
+        // },
 
     ])
     .then((response) => {
-        console.log(response);
-        fs.writeFile(`test.md`, `Hello there!`, (error) => 
-        error ? console.error(error) : console.log(`Success`)
+        const MDgeneration = generateMarkdown(response);
+
+        fs.writeFile(`${response.filename}.md`, MDgeneration, (error) =>
+            error ? console.error(error) : console.log(`Success`)
         );
+        console.log('hell0', MDgeneration);
     })
+
 
